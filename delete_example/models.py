55e2677a4967bf60b1e7d0b8fb48f103490d116f
RandyM1 <randy@travelperk.com>
2019-07-27 16:41:31 +0200

    Motivating Example

--- delete_example/models.py
+++ delete_example/models.py
@@ -1,26 +1,34 @@
+from django.db.models.signals import pre_delete
 from django.db import models
 
 
-class ReportDeleteModel(models.Model):
-    def delete(self, *args, **kwargs):
-        print(f'Delete called on {self.__class__.__name__} {self.id}')
-        return super().delete(*args, **kwargs)
+#  Motivating Example
+class OrderReport(models.Model):
+    pass
+
+
+class Order(models.Model):
+    report = models.ForeignKey(OrderReport, related_name='orders', on_delete=models.CASCADE, null=True)
+
+
+class OrderLine(models.Model):
+    order = models.ForeignKey(Order, related_name='order_lines', on_delete=models.CASCADE, null=True)
 
 
 #  Many to Many
-class ManyToManyParent(ReportDeleteModel):
+class ManyToManyParent(models.Model):
     def print_relations(self):
         print(f'{self.__class__.__name__} id: {self.id} has children '
               f'{[f"{child.__class__.__name__} {child.id}" for child in self.children.all()]}')
 
 
-class ManyToManyChild(ReportDeleteModel):
+class ManyToManyChild(models.Model):
     parents = models.ManyToManyField(ManyToManyParent,
                                      related_name='children')
 
 
 # Many to One
-class ManyToOneParent(ReportDeleteModel):
+class ManyToOneParent(models.Model):
     def print_relations(self):
         print(f'{self.__class__.__name__} id: {self.id} has cascade children '
               f'{[f"{child.__class__.__name__} {child.id}" for child in self.children_cascade.all()]}')
@@ -32,27 +40,27 @@ class ManyToOneParent(ReportDeleteModel):
               f'{[f"{child.__class__.__name__} {child.id}" for child in self.children_protect.all()]}')
 
 
-class ManyToOneChildCascade(ReportDeleteModel):
+class ManyToOneChildCascade(models.Model):
     parent = models.ForeignKey(ManyToOneParent,
                                related_name='children_cascade',
                                on_delete=models.CASCADE)  # Default is CASCADE
 
 
-class ManyToOneChildNull(ReportDeleteModel):
+class ManyToOneChildNull(models.Model):
     parent = models.ForeignKey(ManyToOneParent,
                                related_name='children_null',
                                on_delete=models.SET_NULL,
                                null=True)  # Allows field to be nullable, required for on_delete=SET_NULL
 
 
-class ManyToOneChildProtect(ReportDeleteModel):
+class ManyToOneChildProtect(models.Model):
     parent = models.ForeignKey(ManyToOneParent,
                                related_name='children_protect',
                                on_delete=models.PROTECT)
 
 
 # One to One
-class OneToOneParent(ReportDeleteModel):
+class OneToOneParent(models.Model):
     def print_relations(self):
         if hasattr(self, 'child_cascade'):
             print(f'{self.__class__.__name__} id: {self.id} has cascade child '
@@ -73,20 +81,32 @@ class OneToOneParent(ReportDeleteModel):
             print(f'{self.__class__.__name__} id: {self.id} has no protect child')
 
 
-class OneToOneChildCascade(ReportDeleteModel):
+class OneToOneChildCascade(models.Model):
     parent = models.OneToOneField(OneToOneParent,
                                   related_name='child_cascade',
                                   on_delete=models.CASCADE)  # Default is CASCADE
 
 
-class OneToOneChildNull(ReportDeleteModel):
+class OneToOneChildNull(models.Model):
     parent = models.OneToOneField(OneToOneParent,
                                   related_name='child_null',
                                   on_delete=models.SET_NULL,
                                   null=True)  # Allows field to be nullable, required for on_delete=SET_NULL
 
 
-class OneToOneChildProtect(ReportDeleteModel):
+class OneToOneChildProtect(models.Model):
     parent = models.OneToOneField(OneToOneParent,
                                   related_name='child_protect',
                                   on_delete=models.PROTECT)
+
+
+def report_deletion(sender, **kwargs):
+    instance = kwargs['instance']
+    print(f'Delete called on {instance.__class__.__name__} {instance.id}')
+
+
+for model in [OrderReport, Order, OrderLine,
+              ManyToManyParent, ManyToManyChild,
+              ManyToOneParent, ManyToOneChildCascade, ManyToOneChildNull, ManyToOneChildProtect,
+              OneToOneParent, OneToOneChildCascade, OneToOneChildNull, OneToOneChildProtect]:
+    pre_delete.connect(report_deletion, sender=model)
